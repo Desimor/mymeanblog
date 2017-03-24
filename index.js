e39246fbf3c66e27ab8a52947e8f706af215be90
Matthew Morais <moraismatthew@yahoo.com>
2017-03-23 20:34:08 -0400

    Modified indexJS Added 2 variables called userRouter and postRouter as links to the files. Then assigned server to use both variables.

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ var server = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
+var userRouter = require('./routers/user.router');
+var postRouter = require('./routers/post.router');
+
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./secrets').mongoURI;
 
+server.use(express.static(__dirname + '/public'));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect(mongoURI);
 
-server.use(express.static(__dirname + '/public'));
-
 server.get('/', function(req, res){
     res.sendFile('index.html', {root: __dirname + '/public/html'});
 });
+
+server.use(userRouter);
+server.use(postRouter);
 
 server.listen(port, function(){
     console.log('Now listening on port', port);
