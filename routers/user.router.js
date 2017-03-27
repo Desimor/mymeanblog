2890a5108bf9058b53e7b9d271559f21730354e9
Matthew Morais <moraismatthew@yahoo.com>
2017-03-27 18:26:46 -0400

    Added User POST Methods Added User POST methods for logging in and signing up.

--- routers/user.router.js
+++ routers/user.router.js
@@ -2,6 +2,25 @@ var express = require('express');
 var router = express.Router();
 var Post = require('../models/post.model');
 
+router.post('/signup', function(req, res){
+    var user = new User(req.body);
+    user.save(function(err){
+        if(err){
+            res.status(500).json({
+                msg: err
+            });
+        } else {
+            res.status(201).json({
+                msg: 'You successfully signed up'
+            });
+        }
+    });
+});
+router.post('/login', function(req, res){
+    res.status(200).json({
+        msg: 'Logging in'
+    });
+});
 router.get('/posts', function(req, res){
     Post.find({}, function(err, posts){
         if(err){
