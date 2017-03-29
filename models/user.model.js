5a7e4ba15940212138c20c66b25b4635b6a4833d
Desimor <moraismatthew@gmail.com>
2017-03-29 19:00:33 -0400

    Added Helper Methods Replaced password with salt and hash properties. Added helper methods to set password (create salt and has), validate password, and generate the json web token.

--- models/user.model.js
+++ models/user.model.js
@@ -1,20 +1,27 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-  email:{
-    type: String,
-    required: true,
-    unique: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    hash: {
+        type: String
+    }
 });
+
+userSchema.methods.setPassword = function(password) {}
+userSchema.methods.validPassword = function(password) {}
+userSchema.methods.generateJwt = function() {}
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
